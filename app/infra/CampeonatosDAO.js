0f4fc03f04fa816e8e3539b8a64566ed6235cc41
fernandoamvj <fernandoamvj@gmail.com>
2016-06-20 23:12:01 -0300

    1.3

--- app/infra/CampeonatosDAO.js
+++ app/infra/CampeonatosDAO.js
@@ -1,6 +1,10 @@
 function CampeonatosDAO(connection){//CLasse
     this._connection = connection;//_ deixar claro q connection é privado
 }
+CampeonatosDAO.prototype.verificaUser = function(user,callback){ //usando orientação a objeto em Javascript utiliza-se prototype para adicionar um método ou propriedades a uma classe já criada
+    this._connection.query('select login,senha from ADM where login = ?', user.login, callback);
+    //this._connection.query('select * from ADM', callback);
+}
 CampeonatosDAO.prototype.verificaLogin = function(user,callback){ //usando orientação a objeto em Javascript utiliza-se prototype para adicionar um método ou propriedades a uma classe já criada
     this._connection.query('select id_adm,login from ADM where login = ?', user.login, callback);
     //this._connection.query('select * from ADM', callback);
@@ -13,7 +17,7 @@ CampeonatosDAO.prototype.lista = function(callback){ //usando orientação a obj
     this._connection.query('select * from Campeonatos', callback);
 }
 CampeonatosDAO.prototype.salvaUser = function(user,callback){
-  this._connection.query('insert into ADM set ?',user,callback);//o drive do mysql pra node ja esta preparado para receber um JSOn como foi passado
+    this._connection.query('insert into ADM set ?',user,callback);//o drive do mysql pra node ja esta preparado para receber um JSOn como foi passado
 }
 module.exports = function(){
   return CampeonatosDAO;
--- app/routes/home.js
+++ app/routes/home.js
@@ -16,11 +16,32 @@ module.exports = function(app){
       if(err){
         return next(err);
       }
-      console.log(results[0].id_adm);
-      //req.session.id = results[0].id_adm;
+      req.session.adm = results[0].id_adm;
+      res.render('home/homeUser',{dadosUser:dados});
     });
     connection.end();
-    console.log(req.session.id);
-    res.render('home/homeUser',{dadosUser:dados});
+    //console.log(req.session.id);
   });
+  app.post('/home/homeUser',function(req,res){
+    var user = req.body;
+    req.session.login = user.login;
+    var connection = app.infra.connectionFactory();
+    var objetoBanco = new app.infra.CampeonatosDAO(connection);
+    objetoBanco.verificaUser(user,function(err,results){
+      if(err){
+        return next(err);
+      }
+      if(results!= ''){
+        if((results[0].login == user.login) && (results[0].senha == user.senha)){//results retorna um array de objetos
+
+          res.redirect('/home/homeUser');
+        }
+    }
+      connection.end();
+    //console.log(req.session.id);
+  });
+});
+app.get('/cadastroCamp',function(req,res){
+  res.render('/campeonatos/cadastro');
+});
 }
