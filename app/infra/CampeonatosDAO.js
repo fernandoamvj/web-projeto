8785efcb40cddcd03190e3ac229edf442579bac8
fernandoamvj <fernandoamvj@gmail.com>
2016-06-23 09:19:29 -0300

    final

--- app/infra/CampeonatosDAO.js
+++ app/infra/CampeonatosDAO.js
@@ -19,6 +19,17 @@ CampeonatosDAO.prototype.lista = function(callback) { //usando orientação a ob
 CampeonatosDAO.prototype.salvaUser = function(user, callback) {
     this._connection.query('insert into ADM set ?', user, callback); //o drive do mysql pra node ja esta preparado para receber um JSOn como foi passado
 }
+CampeonatosDAO.prototype.cadastraCamp=function(camp, callback){
+  this._connection.query('insert into Campeonatos set ?',camp,callback);
+}
+CampeonatosDAO.prototype.nEquipes = function(user, callback) { //usando orientação a objeto em Javascript utiliza-se prototype para adicionar um método ou propriedades a uma classe já criada
+    this._connection.query('select id_camp,nEquipes from Campeonatos where id_adm = ?', user.id_adm, callback);
+    //this._connection.query('select * from ADM', callback);
+}
+CampeonatosDAO.prototype.cadastraTimes=function(times, callback){
+    console.log('test')
+    this._connection.query('insert into Times set ?',times,callback);
+}
 module.exports = function() {
     return CampeonatosDAO;
 }
--- app/routes/campeonatos.js
+++ app/routes/campeonatos.js
@@ -1,5 +1,59 @@
-module.exports = function(app){
-  app.post('/home/homeUser/cadastraTimes',function(req,res){
-    console.log('test');
-  });
+module.exports = function(app) {
+    app.post('/home/homeUser/cadastraCamp', function(req, res) {
+        var camp = req.body;
+        camp.id_adm = req.session.adm;
+        //camp.id_adm=req.session.id;
+        var connection = app.infra.connectionFactory();
+        var objetoBanco = new app.infra.CampeonatosDAO(connection);
+        objetoBanco.cadastraCamp(camp, function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/home/User/cadastraEquipes');
+        });
+        connection.end();
+    });
+
+    app.get('/home/homeUser/cadastraEquipes', function(req, res) {
+        var user = {
+            id_adm: req.session.adm
+        };
+        var qEquipes = [{
+            nEquipes: 0
+        }];
+        var connection = app.infra.connectionFactory();
+        var objetoBanco = new app.infra.CampeonatosDAO(connection);
+        objetoBanco.nEquipes(user, function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            var campRecente = results.length - 1;
+            qEquipes[0].nEquipes = results[campRecente].nEquipes;
+            req.session.camp = results[campRecente].id_camp;
+            res.render('campeonatos/cadastraTimes', {
+                equipes: qEquipes
+            });
+        });
+        connection.end();
+    });
+    app.post('/home/homeUser/cadastraTimes', function(req) {
+        var nomeTime = req.body;
+        var times = [{}];
+        var connection = app.infra.connectionFactory();
+        var objetoBanco = new app.infra.CampeonatosDAO(connection);
+        for(var i=0; i < nomeTime.nome.length; i++) {
+            times[i].nome = nomeTime.nome[i];
+            times[i].ativo = '1';
+            times[i].pontuacao = 0;
+            times[i].id_camp = req.session.camp;
+            co
+            objetoBanco.cadastraTimes(times[i], function(err, results) {
+                if (err) {
+                    return next(err);
+                }
+
+            });
+        }
+        connection.end();
+    });
 }
